9c4edca09b0c692fd6eb8af9a82e74c4f2841655
Ayush Patel <patelayushap1512@gmail.com>
2022-08-17 13:29:47 +0530

    Added all the requirements fro heroku Deployment

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# CryptoCoins
+
+It contains the top-12 cryptocurrencies by MCAP
--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def get_home_crypto_list():
     return render_template("home.html", crypto_list=crypto_lists)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run()
